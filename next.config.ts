3a3b320223c68bf17344ea3071fc945f7c6de63f
BruceAuHK <chunkau5-c@my.cityu.edu.hk>
2025-04-27 20:00:40 +0800

    fix(config): Add puppeteer packages to serverComponentsExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@sparticuz/chromium',
+      'puppeteer-core',
+    ],
+  },
   /* config options here */
 };
 
